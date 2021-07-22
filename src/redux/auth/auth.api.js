6698a0ad06285cfaec7d0c8ac3b558d26c83dc44
VazgenBagratunyan9 <vbagratunyan2020@gmail.com>
2021-07-22 21:06:34 +0400

    Updates

--- src/redux/auth/auth.api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const authApi = {
-    getUser(userInputData) {
-        return axios.post("auth/login", userInputData);
-    },
-};
-
-export default authApi;
--- src/redux/common/common.types.js
+++
@@ -1,6 +0,0 @@
-const commonTypes = {
-    TOGGLE_APP_LOADER: "common/TOGGLE_APP_LOADER",
-    SET_GLOBAL_ERROR_MESSAGE: "common/SET_GLOBAL_ERROR_MESSAGE",
-};
-
-export default commonTypes;
